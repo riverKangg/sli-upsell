784099e44b2cca136bb1ffe66d3506e0fe9f9606
riverKangg <riverKangg@gmail.com>
2023-10-16 13:25:35 +0900

    Added hyperparameters utility.

---
+++ utills/hyperparameters.py
@@ -0,0 +1,27 @@
+def get_xgb_hyperparameters():
+    xgb_params = {
+        'max_depth': (3, 10),
+        'learning_rate': (0.01, 0.2),
+        'n_estimators': (10, 300),
+        'min_child_weight': (0, 10),
+        'subsample': (0.5, 1),
+        'colsample_bytree': (0.5, 1),
+        'reg_alpha': (0, 10),
+        'reg_lambda': (0, 10),
+        'gamma': (0, 10)
+    }
+    return xgb_params
+
+def get_lgb_hyperparameters():
+    lgb_params = {
+        'colsample_bytree': (0.5, 1),
+        'learning_rate': (0.01, 0.2),
+        'max_depth': (3, 20),
+        'min_child_samples': (20, 1000),
+        'n_estimators': (10, 500),
+        'num_leaves': (5, 500),
+        'subsample': (0.5, 1),
+        'reg_alpha': (0, 10),
+        'reg_lambda': (0, 10)
+    }
+    return lgb_params
