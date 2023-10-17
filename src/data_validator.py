6ff56fee2cfb93e6a50df8fb9097ba655482578b
riverKangg <riverKangg@gmail.com>
2023-10-17 12:58:49 +0900

    Split the validation data module for modeling and scoring.

--- src/data_validator.py
+++ src/data_validator.py
@@ -7,7 +7,7 @@ class DataValidator:
     def __init__(self, df):
         self.df = df
 
-    def are_columns_included(self):
+    def are_columns_included(self, lst):
         missing_columns = list(filter(lambda column: column not in self.df.columns, data_keys))
         return len(missing_columns) == 0
 
@@ -21,19 +21,28 @@ class DataValidator:
         is_duplicate = data.duplicated().any()
         return is_duplicate
 
-    def validate_data(self):
+    def validate_modeling_data(self):
+        if not self.are_columns_included(data_keys):
+            return False, "누락된 키값이 있습니다."
+        if not self.check_for_null(data_keys):
+            return False, "키값에 NULL이 포함되어 있습니다."
+        if self.is_duplicate(self.df['계약자고객ID']):
+            return False, "계약자고객ID에 중복이 있습니다."
+        if self.are_columns_included(['PERF']):
+            return False, "타겟 데이터가 없습니다."
+        if not self.check_for_null(['PERF']):
+                return False, "타겟에 NULL이 포함되어 있습니다."
+        return True, "데이터가 유효합니다."
+
+    def validate_score_data(self):
         if not self.are_columns_included():
             return False, "누락된 키값이 있습니다."
         if not self.check_for_null(data_keys):
             return False, "키값에 NULL이 포함되어 있습니다."
         if self.is_duplicate(self.df['계약자고객ID']):
             return False, "계약자고객ID에 중복이 있습니다."
-        if 'PERF' in self.df.columns:
-            if not self.check_for_null(['PERF']):
-                return False, "타겟에 NULL이 포함되어 있습니다."
-
-
-        return True, "데이터가 유효합니다."
+        if not self.are_columns_included(['PERF']):
+            return False, "PERF 삭제 필요합니다."
 
 
 # 모듈을 다른 스크립트에서 사용할 수 있도록 테스트 코드를 작성합니다.
@@ -41,7 +50,7 @@ if __name__ == "__main__":
     df = pd.read_csv('../data/sample_data_202211.csv')
 
     validator = DataValidator(df)
-    is_valid, message = validator.validate_data()
+    is_valid, message = validator.validate_modeling_data()
 
     if is_valid:
         print("데이터가 유효합니다.")
